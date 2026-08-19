2e71a8e28997cdb9e50253f91ceae823f74ac95b
Nidhi Sharma <nidhiiisharma2025@gmail.com>
2026-08-19 13:39:34 +0530

    practiceQuetion1

---
+++ stack2/practiceQuestion1.java
@@ -0,0 +1,60 @@
+import java.util.Stack;
+
+public class practiceQuestion1 {
+    class Node{
+        char data;
+        Node next;
+
+        public Node(char data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+
+    public void addNode(char data){
+        Node newNode = new Node(data);
+
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+
+        tail.next=newNode;
+        tail=newNode;
+    }
+
+    public static boolean checkPalindromeUsingStack(Node head){
+        Stack<Character> s = new Stack<>();
+        Node temp=head;
+        StringBuilder before = new StringBuilder();
+        while(temp!=null){
+            s.push(temp.data);
+            before.append(temp.data);
+            temp=temp.next;
+        }
+
+        StringBuilder after = new StringBuilder();
+        while(!s.isEmpty()){
+            after.append(s.pop());
+        }
+        
+        return before.toString().equals(after.toString());   
+    }
+    public static void main(String args[]){
+        practiceQuestion1 ll = new practiceQuestion1();
+        ll.addNode('A');
+        ll.addNode('B');
+        ll.addNode('C');
+        ll.addNode('B');
+        ll.addNode('A');
+
+        if(checkPalindromeUsingStack(head)){
+            System.out.println("Yes its palindrome");
+        }else{
+            System.out.println("No its not palindrome");
+        }
+    }
+}
