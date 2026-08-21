9d5942938590066ff4db702ba35a6a26d87afecb
Nidhi Sharma <nidhiiisharma2025@gmail.com>
2026-08-21 18:52:56 +0530

    practiceQuetion3

--- stack2/practiceQuestion3.java
+++ stack2/practiceQuestion3.java
@@ -1,24 +1,42 @@
 import java.util.Stack;
 
 public class practiceQuestion3 {
-    public static String decodeString(String s){
-        Stack<Character> stkC = new Stack<>();
-        Stack<Character> stkI = new Stack<>();
+    public static String DecodeString(String s){
+        Stack<Integer> numStk = new Stack<>();
+        Stack<String> StrStk = new Stack<>();
+        int number=0;
+        String current = "";
 
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
-            if(c == '[' || c == Character.isLetter(c)){
-                stkC.push(c);
-            }
-            
+
             if(Character.isDigit(c)){
-                stkI.push(c);
+                number = number * 10 + (c-'0');
+            }else if(c == '['){
+                numStk.push(number);
+                StrStk.push(current);
+
+                number=0;
+                current="";
+            }else if(c == ']'){
+                int repeat = numStk.pop();
+                String prev = StrStk.pop();
+
+                String temp="";
+                for(int j=0; j<repeat; j++){
+                    temp += current;
+                }
+                current = prev+temp;
+            }else{
+                current += c;
             }
         }
+        return current;
+
     }
     public static void main(String args[]){
         String s = "3[b2[v]]L";
 
-        decodeString(s);
+        System.out.println(DecodeString(s));
     }
 }
