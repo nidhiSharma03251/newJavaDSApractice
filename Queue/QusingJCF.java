01b6427d1069cc4262eae621623da10017d4c420
Nidhi Sharma <nidhiiisharma2025@gmail.com>
2026-09-04 16:13:12 +0530

    Queue

--- Queue/QusingJCF.java
+++ Queue/QusingJCF.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class QusingJCF {
     public static void main(String[] args) {
         // Queue<Integer> q = new LinkedList<>();
+        // Queue<Integer> q = new PriorityQueue<>();
         Queue<Integer> q = new ArrayDeque<>();
         q.add(2);
         q.add(3);
---
+++ Queue/dequeJCF.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class dequeJCF {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+        deque.addLast(4);
+        System.out.println(deque);
+        deque.removeFirst();
+        System.out.println(deque);
+        deque.removeLast();
+        System.out.println(deque);
+        System.out.println(deque.getFirst());
+        System.out.println(deque.getLast());
+    }
+}
